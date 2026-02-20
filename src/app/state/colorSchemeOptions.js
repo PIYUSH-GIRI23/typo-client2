37c019703783d5cc3f3b4cc1a4a5bfae701096cd
Piyush Giri <piyushgiri630@gmail.com>
2026-02-20 15:35:56 +0530

    feat : added 5 more color options

--- src/app/state/colorSchemeOptions.js
+++ src/app/state/colorSchemeOptions.js
@@ -25,6 +25,51 @@ const colorSchemeOptions = [
         divColor2: "#504945",
         textColor: "#EBDBB2",
         textColor2: "#FABD2F"
+    },
+    {
+        id: 4,
+        name: "Nord Frost",
+        bgColor: "#2E3440",
+        divColor: "#3B4252",
+        divColor2: "#434C5E",
+        textColor: "#ECEFF4",
+        textColor2: "#88C0D0"
+    },
+    {
+        id: 5,
+        name: "Forest Mint",
+        bgColor: "#0F241B",
+        divColor: "#17362A",
+        divColor2: "#24503F",
+        textColor: "#E8FFF4",
+        textColor2: "#7DE2B8"
+    },
+    {
+        id: 6,
+        name: "Rose Night",
+        bgColor: "#2B1B24",
+        divColor: "#3A2431",
+        divColor2: "#4B2E3F",
+        textColor: "#FFEAF2",
+        textColor2: "#FF78B0"
+    },
+    {
+        id: 7,
+        name: "Solar Light",
+        bgColor: "#FFF8E7",
+        divColor: "#F1E4C9",
+        divColor2: "#E7D5AF",
+        textColor: "#3F2E14",
+        textColor2: "#C47A00"
+    },
+    {
+        id: 8,
+        name: "Lavender Dusk",
+        bgColor: "#1F1A2E",
+        divColor: "#2A2340",
+        divColor2: "#372C55",
+        textColor: "#F3EEFF",
+        textColor2: "#B79CFF"
     }
 
 ];
