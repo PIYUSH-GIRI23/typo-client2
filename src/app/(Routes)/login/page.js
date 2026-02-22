bb571b67d69fa4c1491806070617433383e26cac
Piyush Giri <piyushgiri630@gmail.com>
2026-02-22 22:24:34 +0530

    fix :fix Theme import

--- src/app/(Routes)/login/page.js
+++ src/app/(Routes)/login/page.js
@@ -5,7 +5,7 @@ import { Suspense } from 'react'
 import dynamic from 'next/dynamic'
 import Login from '@/app/components/auth/Login'
 
-const Theme = dynamic(() => import('@/app/components/auth/Theme'), {
+const Theme = dynamic(() => import('@/app/components/Theme'), {
   loading: () => <div className="w-full h-screen bg-linear-to-br from-blue-600 via-purple-600 to-pink-600 flex items-center justify-center"><p className="text-white text-xl">Loading...</p></div>,
   ssr: false 
 })
