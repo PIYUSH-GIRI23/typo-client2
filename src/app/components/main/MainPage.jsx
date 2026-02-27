55cf67c48d875f199f4c7b09e213ecc836bccc6a
Piyush Giri <piyushgiri630@gmail.com>
2026-02-27 21:39:21 +0530

    Feat : Added bail Out feature

---
+++ src/app/components/main/MainPage.jsx
@@ -0,0 +1,44 @@
+"use client"
+
+import {useSelector}  from 'react-redux'
+import Paragraph from '@/app/components/main/typing/Paragraph';
+import Menu from '@/app/components/main/typing/Menu';
+import colorSchemeOptions from "@/app/state/colorSchemeOptions";
+import Score from '@/app/components/main/score/Score';  
+import BailOut from '@/app/components/main/BailOut';
+import {useSyncExternalStore,useMemo, useState} from 'react'
+const MainPage = () => {
+    const isClient = useSyncExternalStore(
+        () => () => {},
+        () => true,
+        () => false
+    )
+    const isTyping = useSelector(state => state.typingdata.isTyping);
+    const [isParagraphFocused, setIsParagraphFocused] = useState(false)
+    const id = useSelector((state) => state.colorscheme.id)
+    const activeTheme = useMemo(
+        () => colorSchemeOptions.find((option) => option.id === id) ?? colorSchemeOptions[0],
+        [id]
+    )
+
+    if (!isClient) return null
+  return (
+    <div className='min-h-[89vh] md:h-[89vh] overflow-y-auto' style={{
+        backgroundColor: activeTheme.bgColor,
+    }}>
+      <BailOut/>
+      {
+        isTyping ?
+        <>
+          <Menu isHidden={isParagraphFocused} />
+          <Paragraph onFocusChange={setIsParagraphFocused} />
+        </>:
+        <>
+          <Score/>
+        </>
+      }
+    </div>
+  )
+}
+
+export default MainPage
