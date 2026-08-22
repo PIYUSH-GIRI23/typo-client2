36c5f583eb1a74e41cd6897fcb04997904af541b
Piyush Giri <piyushgiri630@gmail.com>
2026-08-22 17:30:17 +0530

    minor fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,14 @@ export const metadata = {
   authors: [{ name: "Typo" }],
   creator: "Typo",
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://typo.piyx.me"),
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon.ico', sizes: '32x32', type: 'image/x-icon' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
   robots: {
     index: true,
     follow: true,
